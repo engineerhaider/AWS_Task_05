e0faa5078e9e16d986cf1657dcb83a3e23dc1fcb
Haider Shaghati <haidershaghati@haiders-mbp-2.mynetworksettings.com>
2024-05-04 23:40:49 -0400

    Fifth commit

--- src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
+++ src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
@@ -1,7 +1,9 @@
 package com.javatechie.k8s;
-
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @RestController
